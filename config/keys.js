83c5b8dd0c363e3076b3f70e81dca19893ec3c01
unknown <sank.shetye@gmail.com>
2018-08-24 18:25:15 +0530

    Initial Express Server With Route Files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://sanket:sds123@ds057944.mlab.com:57944/develop"
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MOngoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
