a86331c4e60c5677cd067cbfcfe0c4b7b395e2fe
unknown <sank.shetye@gmail.com>
2018-09-21 13:19:30 +0530

    Keys hidden fro development

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://sanket:sds123@ds057944.mlab.com:57944/develop",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
